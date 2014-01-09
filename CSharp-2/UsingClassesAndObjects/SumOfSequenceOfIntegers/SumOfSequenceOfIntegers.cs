0aa54fc4adc338b093fbb4040e5cf724d48e2f1e
adimitrov93 <a.dimitrov93@gmail.com>
2014-01-09 21:08:03 +0200

    Added few task from UsingClassesAndObjects

---
+++ CSharp-2/UsingClassesAndObjects/SumOfSequenceOfIntegers/SumOfSequenceOfIntegers.cs
@@ -0,0 +1,59 @@
+﻿//You are given a sequence of positive integer values written into a string, separated by spaces. Write a function that reads these values from given string and calculates their sum. Example:
+//		string = "43 68 9 23 318"  result = 461
+
+using System;
+using System.Collections.Generic;
+
+class SumOfSequenceOfIntegers
+{
+    static int[] SplitASequenceOfNumbers(string sequence)       //Returns array of ints containing the numbers from the sequence
+    {
+        List<int> numbersFromSequence = new List<int>();
+        int number = 0;
+
+        for (int i = 0; i < sequence.Length; i++)
+        {
+            if (sequence[i] != ' ')
+            {
+                number *= 10;
+                number += sequence[i] - '0';
+            }
+            else
+            {
+                numbersFromSequence.Add(number);
+                number = 0;
+            }
+        }
+
+        if (number != 0)
+        {
+            numbersFromSequence.Add(number);
+        }
+
+        return numbersFromSequence.ToArray();
+    }
+
+    static int SumOfIntegers(int[] numbers)     //Returns the sum of an array of integers
+    {
+        int sum = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+
+        return sum;
+    }
+
+
+    static void Main()
+    {
+        string sequenceOfIntegers = "50 21 16 201 1014";
+
+        int[] numbersFromSequence = SplitASequenceOfNumbers(sequenceOfIntegers);
+
+        int sum = SumOfIntegers(numbersFromSequence);
+
+        Console.WriteLine("The sum is {0}.", sum);
+    }
+}
---
+++ CSharp-2/UsingClassesAndObjects/SurfaceOfATriangle/SurfaceOfATriangle.cs
@@ -0,0 +1,54 @@
+﻿//Write methods that calculate the surface of a triangle by given:
+//Side and an altitude to it; Three sides; Two sides and an angle between them. Use System.Math.
+
+using System;
+
+class SurfaceOfATriangle
+{
+    static double Power(double baseOfNumber, int power)
+    {
+        double result = 1;
+
+        for (int i = 1; i <= power; i++)
+        {
+            result *= baseOfNumber;
+        }
+
+        return result;
+    }
+
+    static double AreaBySideAndAltitude(double side, double altitude)
+    {
+        double area;
+
+        area = (side * altitude) / 2;
+
+        return area;
+    }
+
+    static double AreaByThreeSides(double a, double b, double c)
+    {
+        double halfPerimeter = (a + b + c) / 2;
+
+        double area = Math.Sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));       //Heron's Formula
+
+        return area;
+    }
+
+    static double AreaByTwoSidesAndAngle(double a, double b, double angleInRadians)
+    {
+        double angleInDegrees = angleInRadians * 0.0174532925;      //We need the angle in radians because Math.Cos works with radians
+        double c = Math.Sqrt(Power(a, 2) + Power(b, 2) - (2 * a * b * Math.Cos(angleInDegrees)));          //The Law of Cosines, we find the third side
+
+        double area = AreaByThreeSides(a, b, c);
+
+        return area;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Area of triangle with side {0} and altitude to it {1} is {2} square meters.", 20, 12, AreaBySideAndAltitude(20, 12));
+        Console.WriteLine("Area of triangle with sides {0}, {1} and {2} is {3} square meters.", 5, 5, 5, AreaByThreeSides(5, 5, 5));
+        Console.WriteLine("Area of triangle with sides {0}, {1} and angle between them {2} is {3} square meters.", 5, 7, 49, AreaByTwoSidesAndAngle(5, 7, 49));
+    }
+}
