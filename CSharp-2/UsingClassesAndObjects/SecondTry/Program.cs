a2fc086d641863ff8bdc12d471dedd886305987f
adimitrov93 <a.dimitrov93@gmail.com>
2014-01-14 23:44:14 +0200

    Finished UsingClassesAndObjects

---
+++ CSharp-2/UsingClassesAndObjects/SecondTry/Program.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static string RemoveEmptySpaces(string someString)
+    {
+        for (int i = 0; i < someString.Length; i++)
+        {
+            if (someString[i] == ' ')
+            {
+                someString = someString.Remove(i, 1);
+            }
+        }
+
+        return someString;
+    }
+
+    static string AddEmptySpaces(string someString)
+    {
+        StringBuilder builder = new StringBuilder(someString);
+
+        for (int i = 0; i < builder.Length; i++)
+        {
+            if (IsOperator(builder[i]))
+            {
+                builder.Insert(i + 1, ' ');
+                i++;
+            }
+            else if (Char.IsDigit(builder[i]))
+            {
+                try
+                {
+                    while (Char.IsDigit(builder[i]) || builder[i] == '.')
+                    {
+                        i++;
+                    }
+
+                    builder.Insert(i, ' ');
+                }
+                catch (IndexOutOfRangeException)
+                {
+                }
+            }
+            else if (builder[i] == ',')
+            {
+                builder.Remove(i, 1);
+                i--;
+            }
+            else if (builder[i] == 'l' && builder[i + 1] == 'n')
+            {
+                builder.Insert(i + 2, ' ');
+                i += 2;
+            }
+            else if (builder[i] == 'p' && builder[i + 1] == 'o' && builder[i + 2] == 'w')
+            {
+                builder.Insert(i + 3, ' ');
+                i += 3;
+            }
+            else if (builder[i] == 's' && builder[i + 1] == 'q' && builder[i + 2] == 'r' && builder[i + 3] == 't')
+            {
+                builder.Insert(i + 4, ' ');
+                i += 4;
+            }
+        }
+
+        return builder.ToString();
+    }
+
+    static bool IsOperator(char character)
+    {
+        bool isOperator;
+
+        if (character == '+' || character == '-' || character == '*' || character == '/' || character == '(' || character == ')')
+        {
+            isOperator = true;
+        }
+        else
+        {
+            isOperator = false;
+        }
+
+        return isOperator;
+    }
+
+    static bool IsOperator(string token)
+    {
+        bool isOperator;
+
+        if (token == "+" || token == "-" || token == "*" || token == "/" || token == "(" || token == ")")
+        {
+            isOperator = true;
+        }
+        else
+        {
+            isOperator = false;
+        }
+
+        return isOperator;
+    }
+
+    static bool IsFunction(string token)
+    {
+        bool IsFunction;
+
+        if (token == "ln" || token == "pow" || token == "sqrt")
+        {
+            IsFunction = true;
+        }
+        else
+        {
+            IsFunction = false;
+        }
+
+        return IsFunction;
+    }
+
+    static double EvaluateFunction(string[] tokens, ref int position)
+    {
+        double result = 0;
+
+        if (tokens[position] == "ln")
+        {
+            result = Math.Log(double.Parse(tokens[position + 2]), Math.E);
+            position += 4;
+        }
+        else if (tokens[position] == "pow")
+        {
+            result = Math.Pow(double.Parse(tokens[position + 2]), double.Parse(tokens[position + 3]));
+            position += 5;
+        }
+        else if (tokens[position] == "sqrt")
+        {
+            result = Math.Sqrt(double.Parse(tokens[position + 2]));
+            position += 4;
+        }
+        else
+        {
+            Console.WriteLine("Invalid expression.");
+            Environment.Exit(0);
+        }
+
+        return result;
+    }
+
+    static void PutIntoStackOrQueue(string token, Queue<string> queue, Stack<string> stack)
+    {
+        if (token == "+")
+        {
+            if (stack.Count != 0)
+            {
+                if (stack.Peek() == "*" || stack.Peek() == "/" || stack.Peek() == "+" || stack.Peek() == "-")
+                {
+                    while (stack.Peek() == "*" || stack.Peek() == "/" || stack.Peek() == "+" || stack.Peek() == "-")
+                    {
+                        queue.Enqueue(stack.Pop());
+
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            stack.Push(token);
+        }
+        else if (token == "-")
+        {
+            if (stack.Count != 0)
+            {
+                if (stack.Peek() == "*" || stack.Peek() == "/" || stack.Peek() == "+" || stack.Peek() == "-")
+                {
+                    while (stack.Peek() == "*" || stack.Peek() == "/" || stack.Peek() == "+" || stack.Peek() == "-")
+                    {
+                        queue.Enqueue(stack.Pop());
+
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            stack.Push(token);
+        }
+        else if (token == "*")
+        {
+            if (stack.Count != 0)
+            {
+                if (stack.Peek() == "*" || stack.Peek() == "/")
+                {
+                    while (stack.Peek() == "*" || stack.Peek() == "/")
+                    {
+                        queue.Enqueue(stack.Pop());
+
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            stack.Push(token);
+        }
+        else if (token == "/")
+        {
+            if (stack.Count != 0)
+            {
+                if (stack.Peek() == "*" || stack.Peek() == "/")
+                {
+                    while (stack.Peek() == "*" || stack.Peek() == "/")
+                    {
+                        queue.Enqueue(stack.Pop());
+
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            stack.Push(token);
+        }
+        else if (token == "(")
+        {
+            stack.Push(token);
+        }
+        else if (token == ")")
+        {
+            while (stack.Peek() != "(")
+            {
+                queue.Enqueue(stack.Pop());
+            }
+
+            stack.Pop();        //removes left bracket
+        }
+    }
+
+    static double EvaluateExpression(Queue<string> queue)
+    {
+        string token;
+        Stack<double> stackOfNumbers = new Stack<double>();
+
+        while (queue.Count > 1)
+        {
+            token = queue.Dequeue();
+
+            if (!IsOperator(token))
+            {
+                stackOfNumbers.Push(double.Parse(token));
+            }
+            else
+            {
+                if (token == "*")
+                {
+                    double op2 = stackOfNumbers.Pop();
+                    double op1 = stackOfNumbers.Pop();
+                    stackOfNumbers.Push(op1 * op2);
+                }
+                else if (token == "/")
+                {
+                    double op2 = stackOfNumbers.Pop();
+                    double op1 = stackOfNumbers.Pop();
+
+                    if (op2 == 0)
+                    {
+                        Console.WriteLine("Division by zero is not allowed!");
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        stackOfNumbers.Push(op1 / op2);
+                    }
+                }
+                else if (token == "+")
+                {
+                    double op2 = stackOfNumbers.Pop();
+                    double op1 = stackOfNumbers.Pop();
+                    stackOfNumbers.Push(op1 + op2);
+                }
+                else if (token == "-")
+                {
+                    double op2 = stackOfNumbers.Pop();
+                    double op1 = stackOfNumbers.Pop();
+                    stackOfNumbers.Push(op1 - op2);
+                }
+            }
+
+            //if (stackOfNumbers.Count == 1 && queue.Count == 0)
+            //{
+            //    Console.WriteLine("Result = {0}", stackOfNumbers.Pop());
+            //    Environment.Exit(0);
+            //}
+        }
+
+        return stackOfNumbers.Pop();
+    }
+
+    static Queue<string> InfixToRPN(string[] tokens)
+    {
+        Queue<string> queue = new Queue<string>();
+        Stack<string> stack = new Stack<string>();
+
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            if (IsOperator(tokens[i]))
+            {
+                PutIntoStackOrQueue(tokens[i], queue, stack);
+            }
+            else if (IsFunction(tokens[i]))
+            {
+                queue.Enqueue(EvaluateFunction(tokens, ref i).ToString());
+            }
+            else
+            {
+                queue.Enqueue(tokens[i]);
+            }
+        }
+
+        foreach (var operatorInStack in stack)      //Empty the stack
+        {
+            queue.Enqueue(operatorInStack);
+        }
+
+        return queue;
+    }
+
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        input = RemoveEmptySpaces(input);
+
+        input = AddEmptySpaces(input);
+
+        string[] tokens = input.Split(' ');
+
+        Queue<string> rpn = InfixToRPN(tokens);
+
+        double result = EvaluateExpression(rpn);
+
+        Console.WriteLine(result);
+    }
+}
---
+++ CSharp-2/UsingClassesAndObjects/WorkingDaysCalculator/WorkingDaysCalculator.cs
@@ -0,0 +1,98 @@
+﻿//Write a method that calculates the number of workdays between today and given date, passed as parameter. Consider that workdays are all days from Monday to Friday except a fixed list of public holidays
+//specified preliminary as array.
+
+using System;
+
+class WorkingDaysCalculator
+{
+    //This work only for 2014. If needed for other year holidays array must be updated
+
+    static DateTime[] holidays = 
+    {
+        new DateTime(2014, 1, 1),
+        new DateTime(2014, 3, 3),
+        new DateTime(2014, 4, 18),
+        new DateTime(2014, 4, 21),
+        new DateTime(2014, 5, 1),
+        new DateTime(2014, 5, 2),
+        new DateTime(2014, 5, 5),
+        new DateTime(2014, 5, 6),
+        new DateTime(2014, 9, 22),
+        new DateTime(2014, 11, 1),
+        new DateTime(2014, 12, 24),
+        new DateTime(2014, 12, 25),
+        new DateTime(2014, 12, 26),
+        new DateTime(2014, 12, 31)
+    };
+
+    static bool IsHoliday(DateTime date)        //Search the database and check if a date is holiday
+    {
+        bool isHoliday = false;
+
+        foreach (var holiday in holidays)
+        {
+            if (date == holiday)
+            {
+                isHoliday = true;
+            }
+        }
+
+        return isHoliday;
+    }
+
+    static int WorkingDays(DateTime endDate)    //Count the number of working days from today to endDate
+    {
+        DateTime currentDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+        int workingDays = 0;
+
+        while (currentDate.CompareTo(endDate) <= 0)
+        {
+            if (!IsHoliday(currentDate) && !currentDate.DayOfWeek.Equals(DayOfWeek.Saturday) && !currentDate.DayOfWeek.Equals(DayOfWeek.Sunday))
+            {
+                workingDays++;
+            }
+
+            currentDate = currentDate.AddDays(1);
+        }
+
+        return workingDays;
+    }
+
+    static string PrintDate(DateTime date)      //Returns formatted string with date
+    {
+        return String.Format("{0}.{1}.{2}", date.Date.Day, date.Date.Month, date.Date.Year);
+    }
+
+    static void Main()
+    {
+        DateTime inputDate = new DateTime();
+
+        //get date
+        try
+        {
+            Console.Write("Enter end date in format dd.mm.yyyy: ");
+            inputDate = DateTime.Parse(Console.ReadLine());
+
+            if (inputDate.CompareTo(DateTime.Now) < 0)
+            {
+                throw new Exception("The date must be at least tommorow.");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("You entered wrong date!");
+            return;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
+
+        //count the working dayss
+        int workingDays = WorkingDays(inputDate);
+
+        //print result
+        Console.WriteLine("From today ({0}) to {1} there are {2} working days.", PrintDate(DateTime.Now), PrintDate(inputDate), workingDays);
+    }
+}
